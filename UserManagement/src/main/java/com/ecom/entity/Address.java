f7a142ecbac0de4cc1f02dda9923632e23835e8c
Omkar-world <omkarverma8052979256@gmail.com>
2025-05-25 01:54:10 +0530

    add entity in UserManagement module by Om

---
+++ UserManagement/src/main/java/com/ecom/entity/Address.java
@@ -0,0 +1,40 @@
+package com.ecom.entity;
+
+import org.springframework.cache.annotation.EnableCaching;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="Address")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Address {
+
+	@Id
+	@SequenceGenerator(name = "Gen2",sequenceName = "Seq_Adddress",initialValue = 1,allocationSize = 1)
+	@GeneratedValue(generator = "Gen2",strategy = GenerationType.SEQUENCE)
+	private Long id;
+	@Column(length = 20)
+	private String city;
+	@Column(length = 20)
+	private String state;
+	@Column(length = 30)
+	private String country;
+	@OneToOne(targetEntity = User.class,cascade = CascadeType.ALL,fetch = FetchType.EAGER,mappedBy = "address")
+	private User user;
+	
+}
---
+++ UserManagement/src/main/java/com/ecom/entity/User.java
@@ -0,0 +1,52 @@
+package com.ecom.entity;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Table(name = "User")
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+	@Id
+	@SequenceGenerator(name = "Gen1",sequenceName ="Seq_User",initialValue = 1,allocationSize = 1)
+	@GeneratedValue(generator = "Gen1",strategy =GenerationType.SEQUENCE)
+	private Long id;
+	@Column(length = 30)
+	private String name;
+	@Column(length = 50)
+	private String email;
+	@Column(length = 20)
+	private String password;
+	@Column(length = 20)
+	private String role;
+	@CreationTimestamp
+	@Column(insertable = true,updatable = false)
+	private LocalDateTime creationTime;
+	@UpdateTimestamp
+	@Column(updatable = true,insertable = false)
+	private LocalDateTime updationTime;
+	@OneToMany(targetEntity = Address.class,cascade = CascadeType.ALL,fetch = FetchType.EAGER)
+	@JoinColumn(name="address_id",referencedColumnName = "id")
+	private List<Address> address;
+}
