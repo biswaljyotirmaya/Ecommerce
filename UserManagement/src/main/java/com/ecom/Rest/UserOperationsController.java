4e063a841f779bf51c4b6ebd835e39b8509d51c8
hrithikranjan <hrithikranjan@192.168.1.11>
2025-05-26 12:32:28 +0530

    just add one API for registerUser

---
+++ UserManagement/src/main/java/com/ecom/Rest/UserOperationsController.java
@@ -0,0 +1,70 @@
+package com.ecom.Rest;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ecom.dto.UserDto;
+import com.ecom.service.IUserService;
+
+
+@RestController
+@RequestMapping("/User")
+class UserOperationsController {
+
+	@Autowired
+	private IUserService UserService;
+	
+	
+    //public String registerUser(UserDto userDto)
+    
+    @PostMapping("/register")
+	public ResponseEntity<String> registerUser(UserDto userDto)
+	{
+		try {
+			
+			String msg = UserService.registerUser(userDto);
+			
+			return new ResponseEntity<String>(msg,HttpStatus.CREATED);
+			
+		}catch(Exception e)
+		{
+			return new ResponseEntity<String>("Internal Problem Occurs",HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		
+	}
+//    // List<UserDto> getAllUsers();
+//    
+//    public ResponseEntity<List> fetchAllUser()
+//    {
+//    	
+//    	
+//    	
+//    }
+//    
+//    
+//    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+	
+	
+}
