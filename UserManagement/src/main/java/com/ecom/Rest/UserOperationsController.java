c628d6dc9571427c1eb44f2e88e739b8cba6aa36
hrithikranjan <hrithikranjan@192.168.1.11>
2025-05-26 13:35:54 +0530

    registerUser , show all User and Update Usr done

--- UserManagement/src/main/java/com/ecom/Rest/UserOperationsController.java
+++ UserManagement/src/main/java/com/ecom/Rest/UserOperationsController.java
@@ -5,12 +5,14 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ecom.dto.UserDto;
-import com.ecom.entity.User;
 import com.ecom.service.IUserService;
 
 
@@ -40,7 +42,7 @@ class UserOperationsController {
 		
 	}
 
-    
+    @GetMapping("/allUser")
     public ResponseEntity<?> fetchAllUser()
     {
     	try
@@ -58,6 +60,25 @@ class UserOperationsController {
     	}
     }
     
+//    // UserDto updateUser(Long userId, UserDto userDto);
+    
+    @PutMapping("/modify/{id}")
+   public ResponseEntity<String> modifyUser(@PathVariable("id") Long id,UserDto uDto)
+    {
+    	
+    	try {
+    	
+    		UserService.updateUser(id, uDto);
+    		return new ResponseEntity<String>("User Detalils Updated",HttpStatus.OK);
+    		
+    		
+    	}
+    	catch(Exception e)
+    	{
+    		return new ResponseEntity<String>("Problem Occur for Updation",HttpStatus.INTERNAL_SERVER_ERROR);
+    	}
+    	
+    }
     
     
     
