b208f5964a6d4fd3a371ff81d4704cf6b5f81151
Omkar-world <omkarverma8052979256@gmail.com>
2025-06-08 12:58:13 +0530

    commit By Om

--- ProductService/src/main/java/com/ecom/service/IProductMgmtService.java
+++ ProductService/src/main/java/com/ecom/service/IProductMgmtService.java
@@ -3,10 +3,13 @@ package com.ecom.service;
 import java.util.List;
 
 import com.ecom.dto.ProductDto;
+import com.ecom.entity.Brand;
+import com.ecom.entity.Category;
 
 public interface IProductMgmtService {
 
 	public String saveProduct(ProductDto dto);
 	public List<ProductDto> getAllProducts();
 	public ProductDto findProductById(Long id);
+	public List<ProductDto> findByNameAndCategoryaAndBrand(String name,Category category,Brand brand);
 }
--- ProductService/src/main/java/com/ecom/service/ProductMgmtServiceImpl.java
+++ ProductService/src/main/java/com/ecom/service/ProductMgmtServiceImpl.java
@@ -7,7 +7,7 @@ import java.util.Optional;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.domain.Example;
 import org.springframework.stereotype.Service;
 
 import com.ecom.config.AppConfig;
@@ -22,7 +22,10 @@ import com.ecom.repository.IBrandRepository;
 import com.ecom.repository.ICategoryRepository;
 import com.ecom.repository.IProductReository;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Service
+@Slf4j
 public class ProductMgmtServiceImpl implements IProductMgmtService {
 
 	@Autowired
@@ -40,17 +43,21 @@ public class ProductMgmtServiceImpl implements IProductMgmtService {
 
 	@Override
 	public String saveProduct(ProductDto dto) {
+		log.info("Beafore Object creation");
 		Product productEntity = new Product();
 		Brand brandEntity = new Brand();
 		Category categoryEntity = new Category();
+		log.info("After Object creation");
 		BrandDto brandDto = dto.getBrand();
 		CategoryDto categoryDto = dto.getCategory();
 		BeanUtils.copyProperties(dto, productEntity);
 		BeanUtils.copyProperties(categoryDto, categoryEntity);
 		BeanUtils.copyProperties(brandDto, brandDto);
 		categoryRepo.save(categoryEntity);
+		log.info("Before save method call");
 		brandRepo.save(brandEntity);
 		productEntity = productRepo.save(productEntity);
+		log.info("After save method call");
 		return productEntity.getId() != null ? msg.get(ProductConstant.SAVE_SUCCESS) + productEntity.getId()
 				: msg.get(ProductConstant.SAVE_FAILURE);
 	}
@@ -58,6 +65,7 @@ public class ProductMgmtServiceImpl implements IProductMgmtService {
 	@Override
 	public List<ProductDto> getAllProducts() {
 		List<ProductDto> dto = new ArrayList<>();
+		log.info("Beafore findAll() method");
 		List<Product> list = productRepo.findAll();
 		list.forEach((li) -> {
 			ProductDto productDto = new ProductDto();
@@ -80,5 +88,28 @@ public class ProductMgmtServiceImpl implements IProductMgmtService {
 		}
 		return dto;
 	}
+	
+	@Override
+	public List<ProductDto> findByNameAndCategoryaAndBrand(String name, Category category, Brand brand) {
+		
+	log.info("Before Product object creation");
+	Product product=new Product();
+	product.setBrand(brand);
+	product.setName(name);
+	product.setCategory(category);
+	Example<Product> example=Example.of(product);
+	log.info("Beafore findAll() method call");
+	List<Product> list=productRepo.findAll(example);
+	List<ProductDto> dtoList=new ArrayList<>();
+	if(list.size()<=0|| list==null) {
+		log.warn("list is null");
+		return null;
+	}
+	
+		BeanUtils.copyProperties(list, dtoList);
+		return dtoList;
+	}
+	
+	
 
 }
